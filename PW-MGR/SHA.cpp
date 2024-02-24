619239721d8fb9df5597b91d50e25dfcdf647143
onyon371 <onyon371@gmail.com>
2024-02-24 15:00:29 +0100

    rotr

--- PW-MGR/SHA.cpp
+++ PW-MGR/SHA.cpp
@@ -1,1 +1,12 @@
 #include "SHA.h"
+
+void rotr(vector<int>& input, int n)
+{
+    int dim = input.size();
+    for (int i = 0; i < dim; i++)
+    {
+        if (i + n < dim) swap(input[i], input[i + n]);
+        else swap(input[i], input[i + n - dim]);
+    }
+
+}
--- PW-MGR/SHA.h
+++ PW-MGR/SHA.h
@@ -1,2 +1,4 @@
 #pragma once
 #include "include.h"
+
+void rotr(vector<int>& input, int n);
